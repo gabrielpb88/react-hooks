58f87e2c4b810178036d84b03fba70d613d3a51b
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2021-06-13 14:12:01 -0300

    adicionado useEffect

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,37 +1,35 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import DadosPessoais from './DadosPessoais'
 import DadosUsuario from './DadosUsuario'
 import DadosEntrega from './DadosEntrega'
-import { Typography } from '@material-ui/core'
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
 
   const [etapaAtual, setEtapaAtual] = useState(0)
+  const [dadosColetados, setDados] = useState({})
+
+  useEffect(()=>{
+    console.log(dadosColetados)
+  })
+
+  function coletarDados(dados) {
+    setDados({...dadosColetados, ...dados})
+    proximo()
+  }
 
   function proximo() {
     setEtapaAtual(etapaAtual + 1)
   }
 
-  function formularioAtual(estadoAtual) {
-    switch (estadoAtual) {
-      case 0:
-        return <DadosUsuario aoEnviar={proximo} />
-      case 1:
-        return <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />
-      case 2:
-        return <DadosEntrega aoEnviar={aoEnviar} />
-      default:
-        return <Typography>Erro ao selecionar formulário</Typography>
-    }
-  }
+  const formularios = [
+    <DadosUsuario aoEnviar={coletarDados} />,
+    <DadosPessoais aoEnviar={coletarDados} validarCPF={validarCPF} />,
+    <DadosEntrega aoEnviar={coletarDados} />
+  ]
 
   return (
-    <>
-      {
-        formularioAtual(etapaAtual)
-      }
-    </>
+    <>{formularios[etapaAtual]}</>
   )
 }
 
