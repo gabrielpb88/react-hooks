1a2030343c6936985aa2c9b4feeddb1d9f8c9a89
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2021-06-14 23:10:39 -0300

    adicionado validacao

--- src/components/FormularioCadastro/DadosUsuario.jsx
+++ src/components/FormularioCadastro/DadosUsuario.jsx
@@ -1,20 +1,40 @@
 import React, { useState } from 'react'
 import { Button, TextField } from '@material-ui/core'
 
-function DadosUsuario({ aoEnviar }) {
+function DadosUsuario({ aoEnviar, validacoes }) {
   const [email, setEmail] = useState('')
   const [senha, setSenha] = useState('')
+  const [erros, setErros] = useState({ senha: { valido: true, texto: '' } })
+
+  function validarCampos(event) {
+    const {name, value} = event.target
+    const novoEstado = {...erros}
+    novoEstado[name] = validacoes[name](value)
+    setErros(novoEstado)
+  }
+
+  function possoEnviar() {
+    for(let campo in erros){
+      if(!erros[campo].valido){
+        return false
+      }
+    }
+    return true
+  }
 
   return (
     <form onSubmit={(event) => {
       event.preventDefault()
-      aoEnviar({email, senha})
+      if(possoEnviar()){
+        aoEnviar({email, senha})
+      }
     }}>
       <TextField
         value={email}
         onChange={e => setEmail(e.target.value)}
         id='email'
         label='email'
+        name='email'
         type='email'
         required
         variant='outlined'
@@ -24,8 +44,12 @@ function DadosUsuario({ aoEnviar }) {
       <TextField
         value={senha}
         onChange={e => setSenha(e.target.value)}
+        onBlur={validarCampos}
+        error={!erros.senha.valido}
+        helperText={erros.senha.texto}
         id='senha'
         label='senha'
+        name='senha'
         type='password'
         required
         variant='outlined'
